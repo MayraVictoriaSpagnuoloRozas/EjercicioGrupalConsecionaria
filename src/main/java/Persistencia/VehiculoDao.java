f317dd9171a3192bb2236e1e8680c3d05f782c71
Claudia Ramos <cramos@fi.uba.ar>
2023-04-14 19:49:53 -0300

    Se agregaron las clases Dao y VehiculoDao

---
+++ src/main/java/Persistencia/VehiculoDao.java
@@ -0,0 +1,39 @@
+package Persistencia;
+
+import Entidad.Vehiculo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VehiculoDao extends Dao {
+
+    public List<Vehiculo> getVehiculo() throws Exception {
+        try {
+            String sql = "SELECT * FROM informacion;";
+
+            queryDatabase(sql);
+
+            List<Vehiculo> vehiculos = new ArrayList<>();
+            Vehiculo vehiculo;
+
+            while (resultSet.next()) {
+                vehiculo = new Vehiculo();
+
+                vehiculo.setMarca(resultSet.getString(2));
+                vehiculo.setModelo(resultSet.getString(3));
+                vehiculo.setColor(resultSet.getString(4));
+                vehiculo.setStock(resultSet.getInt(5));
+
+                vehiculos.add(vehiculo);
+            }
+
+            return vehiculos;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new Exception("Error al obtener los vehiculos");
+        } finally {
+            disconnectDatabase();
+        }
+    }
+}
